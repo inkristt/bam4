53506184421a49504d1466efbbbaea032cfb62cc
Milos Tomic <milostomic502@gmail.com>
2022-07-26 23:41:25 +0200

    kraj4

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,36 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
-import { AiOutlineShopping, AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineShopping,AiOutlineMenu } from 'react-icons/ai'
 
 import Korpa from './Korpa';
 import { useStateContext } from '../context/StateContext';
 import Menu from './Menu';
 import Image from "next/image"
-import logo from "../pages/assets/logo.png";
-import Router from "next/router";
 const Navigation = () => {
-  const { showCart, setShowCart, totalQuantities, showMenu, setshowMenu } = useStateContext();
+  const { showCart, setShowCart, totalQuantities ,showMenu,setshowMenu} = useStateContext();
 
   return (
     <div className="navbar-container">
       <button type="button" onClick={() => setshowMenu(!showMenu)} className="cart-icon" >
         <AiOutlineMenu />
       </button>
-     
-     <Image src={logo} alt="hhh" width={40} height={40} />
-    
-      
-      
+      <p className="logo">
+        <Link href="/">Srebro Shop</Link>
+      </p>
 
       <button type="button" onClick={() => setShowCart(true)} className="cart-icon" >
         <AiOutlineShopping />
         <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+      
+      {showMenu && <Menu/>}
 
-      {showMenu && <Menu />}
 
-
-      {showCart && <Korpa />}
+      {showCart &&<Korpa/>}
     </div>
   )
 }
