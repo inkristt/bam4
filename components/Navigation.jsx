853ca10078252dd53bc6fe71c9bd51b25113ab2f
Milos Tomic <milostomic502@gmail.com>
2022-07-26 22:09:33 +0200

    kraj4

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -5,19 +5,17 @@ import { AiOutlineShopping,AiOutlineMenu } from 'react-icons/ai'
 import Korpa from './Korpa';
 import { useStateContext } from '../context/StateContext';
 import Menu from './Menu';
-import logo from '../pages/assets/logo.png'
-import Image from 'next/image'
-import Router from "next/router";
+
 const Navigation = () => {
   const { showCart, setShowCart, totalQuantities ,showMenu,setshowMenu} = useStateContext();
-  
+
   return (
     <div className="navbar-container">
       <button type="button" onClick={() => setshowMenu(!showMenu)} className="cart-icon" >
         <AiOutlineMenu />
       </button>
-      <p className="logo" onClick={()=> Router.push("/")}>
-            Srebro Shop
+      <p className="logo">
+        <Link href="/">Srebro Shop</Link>
       </p>
 
       <button type="button" onClick={() => setShowCart(true)} className="cart-icon" >
