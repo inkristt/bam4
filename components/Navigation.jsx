55fe9994a901891869e5684ce314854ea76207dd
Milos Tomic <milostomic502@gmail.com>
2022-07-26 23:25:17 +0200

    kraj4

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -17,9 +17,9 @@ const Navigation = () => {
       </button>
       <p className="logo">
         <Link href="/">
-          <>
-            <Image src={logo} alt="" width={50} height={50} />
-          </>
+          <a>
+          <Image src={logo} alt="" width={40} height={40} />
+          </a>
         </Link>
       </p>
 
