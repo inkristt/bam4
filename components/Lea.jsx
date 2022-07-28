29171ae3514e0d8975d3914e81d420de7969b624
Milos Tomic <milostomic502@gmail.com>
2022-07-28 19:25:28 +0200

    lol4

--- components/Lea.jsx
+++ components/Lea.jsx
@@ -14,6 +14,7 @@ const Layout = ({ children }) => {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
         <meta name="theme-color" content="#ffffff" />
         <title>Srebro Shop</title>
       </Head>  
