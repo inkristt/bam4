76508c4f945e6c5a6ef3ead700f9bb4d184ebfa0
Milos Tomic <milostomic502@gmail.com>
2022-07-28 16:56:18 +0200

    search4

--- components/Gototop.jsx
+++ components/Gototop.jsx
@@ -27,7 +27,7 @@ const Gototop = () => {
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.5 }}
-                className='top'>
+                className='top1'>
                 <AiOutlineArrowUp />
             </motion.div>}
         </>
